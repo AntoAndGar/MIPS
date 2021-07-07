8225510ade6ce2750f31771a33803e3ed1b626bb
antoandgar <antonioandgargiulo@gmail.com>
2021-07-07 14:34:58 +0200

    start of ex 8.2

--- EserciziMips/8.2-SimilitudineStringhe.asm
+++ EserciziMips/8.2-SimilitudineStringhe.asm
@@ -9,6 +9,67 @@
 .globl main
 
 main:
+	la $a0, str1
+	li $a1, 255
+	li $v0, 8
+	syscall
 	
+	jal STRLEN
+	move $t0, $v0
+	
+	la $a0, str2
+	li $a1, 255
+	li $v0, 8
+	syscall
+	
+	subi $sp, $sp, 4
+	sw $t0, 0($sp)
+	jal STRLEN
+	move $t1, $v0
+	lw $t0 0($sp)
+	addi $sp, $sp, 4
+	
+	la $a0, str1
+	la $a1, str2
+	move $a2, $t0
+	move $a3, $t1
+	jal SIMILITUDINE
+	
+	move $a0, $v0
+	
+	li $v0, 1
+	syscall
+	
+	li $v0, 10
+	syscall
+	
+STRLEN:
+	#a0 address of string
+	move $t0, $a0
+	move $t1, $a0
+len_loop:
+	lb $t2, 0($t0)
+	beq $t2, $zero, len_end
+	addi $t0, $t0, 1
+	j len_loop
+len_end:
+	sub $t3, $t0, $t1
+	move $v0, $t3
+	jr $ra
+	
+
+SIMILITUDINE:
+	# a0, a1 addresses of strings
+	# a2, a3 length of strings
+	move $t0, $a0
+	move $t1, $a1
+	
+	li $t2, 0
+loop:
+	ble $t
+	addi $t2, $t2, 1
+	j loop
 	
 .data
+str1: .space 255
+str2: .space 255
